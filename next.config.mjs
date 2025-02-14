24e3e76f838accbad2e52ecf1896d68fbdfb9d84
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-14 21:33:41 +0000

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production"; // Check if in production
-
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/React-POS" : "", // Add your repository name here
-  assetPrefix: isProd ? "/React-POS/" : "",
+  assetPrefix: "./", // Ensure assets are loaded from the correct relative path
   images: {
     remotePatterns: [
       {
