e3d103f8216960376407dc7abb16889bb7970e44
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-15 02:18:02 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Add this line for static export
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
