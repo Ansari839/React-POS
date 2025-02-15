aac7651522f8c660261e8ab5daa3dd0316914b80
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-15 14:39:11 +0000

    Chenge Config

--- next.config.mjs
+++ next.config.mjs
@@ -10,17 +10,17 @@
 
 // export default nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/React-POS", // GitHub Pages repo name
-  assetPrefix: "/React-POS/", // GitHub Pages ke liye required
-  images: {
-    unoptimized: true, // GitHub Pages ke liye
-  },
-  trailingSlash: true, // GitHub Pages ke liye necessary
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: "/React-POS", // GitHub Pages repo name
+//   assetPrefix: "/React-POS/", // GitHub Pages ke liye required
+//   images: {
+//     unoptimized: true, // GitHub Pages ke liye
+//   },
+//   trailingSlash: true, // GitHub Pages ke liye necessary
+// };
 
-export default nextConfig;
+// export default nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
@@ -28,3 +28,13 @@ export default nextConfig;
 // };
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Vercel ke liye best option
+  images: {
+    domains: ["https://react-pos-three.vercel.app/"], // Agar external images use kar rahe ho
+  },
+};
+
+export default nextConfig;
