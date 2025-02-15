8450ce427059e153c452a470371a1dc98db79b62
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-15 14:43:40 +0000

    Check Deployment

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="bg-slate-100">Hello Worldss</div>;
+  return (
+    <div className="bg-slate-100">
+      <p>Hello Wordl</p>
+
+      <p>Hello Pakitsan</p>
+      <p>Hello Karachi</p>
+    </div>
+  );
 }
