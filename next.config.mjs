7b290713851abd7b430f8aed98ab369a71648497
Abdullah Ansari <137299911+Ansari839@users.noreply.github.com>
2025-02-15 14:21:11 +0000

    Vercel Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   basePath: "/React-POS", // Your repo name
+//   assetPrefix: "/React-POS/", // Your repo name
+//   images: {
+//     unoptimized: true, // For static export
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/React-POS", // Your repo name
-  assetPrefix: "/React-POS/", // Your repo name
+  basePath: "/React-POS", // GitHub Pages repo name
+  assetPrefix: "/React-POS/", // GitHub Pages ke liye required
   images: {
-    unoptimized: true, // For static export
+    unoptimized: true, // GitHub Pages ke liye
   },
+  trailingSlash: true, // GitHub Pages ke liye necessary
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
