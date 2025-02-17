732f44f78a23ebfbc46a06b29213b6b5fc104eb1
almasansri16 <almashanif126@gmail.com>
2025-02-18 02:54:18 +0500

    Bottom Nav Complete

--- app/page.js
+++ app/page.js
@@ -1,5 +1,18 @@
+import BottonNav from "@/components/BottonNav";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="bg-slate-100">Home</div>;
+  return (
+    <div className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* Left Section */}
+      <div className="flex-[3] bg-red-600">
+        Left
+      </div>
+      {/* Right Section */}
+      <div className="flex-[2] bg-blue-600">
+        Right
+      </div>
+      <BottonNav />
+    </div>
+  );
 }
--- components/Header/Search.jsx
+++ components/Header/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
   const searchStyles = {
     container:
       "flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5 py-2 w-[500px]",
-    input: "bg-[#333] text-[#f5f5f5]",
+    input: "bg-[#1f1f1f] text-[#f5f5f5] outline-none",
   };
 
   return (
